4de3c5292afadcefb99af6d35c5b122ec51a1382
Alex Rudoi <sanikbrant@gmail.com>
2021-05-26 18:45:00 +0300

    build bug fixes 2

--- SmileOnlineProject/AppDelegate.swift
+++ SmileOnlineProject/AppDelegate.swift
@@ -16,26 +16,8 @@ import FBSDKCoreKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate,GIDSignInDelegate {
-<<<<<<< HEAD:SmileOnlineProject/AppDelegate.swift
-    
-    //
-    var window: UIWindow?
-    
-    let gcmMessageIDKey = "gcm.Message_ID"
-    
-    
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        if #available(iOS 10.0, *) {
-            // For iOS 10 display notification (sent via APNS)
-            UNUserNotificationCenter.current().delegate = self
-            
-            
-            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
-            UNUserNotificationCenter.current().requestAuthorization(
-                options: authOptions,
-                completionHandler: {_, _ in })
-=======
 
+    //
     var window: UIWindow?
 
     let gcmMessageIDKey = "gcm.Message_ID"
@@ -51,17 +33,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate,GIDSignInDelegate {
           UNUserNotificationCenter.current().requestAuthorization(
             options: authOptions,
             completionHandler: {_, _ in })
->>>>>>> 2d5d69981c3cf2ac1ee7708806fd39953960a24c:SmileOnlineProject/files/AppDelegate.swift
         } else {
-            let settings: UIUserNotificationSettings =
-                UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
-            application.registerUserNotificationSettings(settings)
+          let settings: UIUserNotificationSettings =
+          UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+          application.registerUserNotificationSettings(settings)
         }
-<<<<<<< HEAD:SmileOnlineProject/AppDelegate.swift
-        
-=======
 
->>>>>>> 2d5d69981c3cf2ac1ee7708806fd39953960a24c:SmileOnlineProject/files/AppDelegate.swift
         application.registerForRemoteNotifications()
 
         Messaging.messaging().delegate = self
@@ -73,30 +50,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate,GIDSignInDelegate {
         //Для входа по гуглпочте
         GIDSignIn.sharedInstance()?.clientID = "135603421643-46htmd4ghqitulpodlrrj3us51l854c5.apps.googleusercontent.com"
         GIDSignIn.sharedInstance()?.delegate = self
-<<<<<<< HEAD:SmileOnlineProject/AppDelegate.swift
-=======
-        
-        getToken()
->>>>>>> 2d5d69981c3cf2ac1ee7708806fd39953960a24c:SmileOnlineProject/files/AppDelegate.swift
         
         getToken()
         
-<<<<<<< HEAD:SmileOnlineProject/AppDelegate.swift
         
-        return true
-    }
-    // Для входа по гуглпочте
-    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
-        print("User \(user.profile.email ?? "No email")")
-    }
-    
-    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
-        return GIDSignIn.sharedInstance().handle(url)
-        
-        
-    }
-    
-=======
         return true
     }
 // Для входа по гуглпочте
@@ -109,6 +66,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate,GIDSignInDelegate {
 
 
     }
+    
     
     
     // MARK: UISceneSession Lifecycle
@@ -124,17 +82,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate,GIDSignInDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
->>>>>>> 2d5d69981c3cf2ac1ee7708806fd39953960a24c:SmileOnlineProject/files/AppDelegate.swift
 
-// MARK: UISceneSession Lifecycle
-
-<<<<<<< HEAD:SmileOnlineProject/AppDelegate.swift
-func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-    // Called when a new scene session is being created.
-    // Use this method to select a configuration to create the new scene with.
-    return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-}
-=======
+    // MARK: - Core Data stack
 var persistentContainer: NSPersistentContainer = {
         /*
          The persistent container for the application. This implementation
@@ -161,65 +110,22 @@ var persistentContainer: NSPersistentContainer = {
         })
         return container
     }()
->>>>>>> 2d5d69981c3cf2ac1ee7708806fd39953960a24c:SmileOnlineProject/files/AppDelegate.swift
 
-func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-    // Called when the user discards a scene session.
-    // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-    // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-}
+    // MARK: - Core Data Saving support
 
-// MARK: - Core Data stack
-var persistentContainer: NSPersistentContainer = {
-    /*
-     The persistent container for the application. This implementation
-     creates and returns a container, having loaded the store for the
-     application to it. This property is optional since there are legitimate
-     error conditions that could cause the creation of the store to fail.
-     */
-    let container = NSPersistentContainer(name: "SmileOnlineProject")
-    container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-        if let error = error as NSError? {
-            // Replace this implementation with code to handle the error appropriately.
-            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-            
-            /*
-             Typical reasons for an error here include:
-             * The parent directory does not exist, cannot be created, or disallows writing.
-             * The persistent store is not accessible, due to permissions or data protection when the device is locked.
-             * The device is out of space.
-             * The store could not be migrated to the current model version.
-             Check the error message to determine what the actual problem was.
-             */
-            fatalError("Unresolved error \(error), \(error.userInfo)")
-        }
-    })
-    return container
-}()
-
-// MARK: - Core Data Saving support
-
-func saveContext () {
-    let context = persistentContainer.viewContext
-    if context.hasChanges {
-        do {
-            try context.save()
-        } catch {
-            // Replace this implementation with code to handle the error appropriately.
-            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-            let nserror = error as NSError
-            fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
         }
     }
-<<<<<<< HEAD:SmileOnlineProject/AppDelegate.swift
-}
-
-func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any],
-                 fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
-    
-    if let messageID = userInfo[gcmMessageIDKey] {
-        print("Message ID: \(messageID)")
-=======
     
     func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any],
                      fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
@@ -262,110 +168,55 @@ func application(_ application: UIApplication, didReceiveRemoteNotification user
         
         
     }
-}
     
-
+}
 extension AppDelegate : MessagingDelegate {
     func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
       print("Firebase registration token: \(String(describing: fcmToken))")
-
-      let dataDict:[String: String] = ["token": fcmToken ?? ""]
-        
-        print(dataDict)
-        
-      NotificationCenter.default.post(name: Notification.Name("FCMToken"), object: nil, userInfo: dataDict)
-      // TODO: If necessary send token to application server.
-      // Note: This callback is fired at each app startup and whenever a new token is generated.
->>>>>>> 2d5d69981c3cf2ac1ee7708806fd39953960a24c:SmileOnlineProject/files/AppDelegate.swift
-    }
-    
-    // Print full message.
-    print(userInfo)
-    
-    completionHandler(UIBackgroundFetchResult.newData)
-}
-<<<<<<< HEAD:SmileOnlineProject/AppDelegate.swift
-
-func getToken(){
-    //скокращение
-    let db = Firestore.firestore()
-    guard let user = UserDefaults.standard.string(forKey: "authID") else {return}
-    
-    Messaging.messaging().token { token, error in
-        if let error = error {
-            print("Error fetching FCM registration token: \(error)")
-        } else if let token = token {
-            print("FCM registration token: \(token)")
-            
-            //Запись клиента в удаленную базу данных
-            db.collection("users")
-                .document("\(user)")
-                .updateData([
-                    "token": token
-                ]) { err in
-                    if let err = err {
-                        print("Error writing document: \(err.localizedDescription)")
-                    } else {
-                        print("Document successfully written!")
-                    }
-                }
-        }
-    }
-    
-    
-}
-}
-
-
-extension AppDelegate : MessagingDelegate {
-    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
-        print("Firebase registration token: \(String(describing: fcmToken))")
         let dataDict:[String: String] = ["token": fcmToken ?? ""]
-        
-        print(dataDict)
-        
-        NotificationCenter.default.post(name: Notification.Name("FCMToken"), object: nil, userInfo: dataDict)
-        // TODO: If necessary send token to application server.
-        // Note: This callback is fired at each app startup and whenever a new token is generated.
-    }
-}
+                
+                print(dataDict)
+                
+              NotificationCenter.default.post(name: Notification.Name("FCMToken"), object: nil, userInfo: dataDict)
+              // TODO: If necessary send token to application server.
+              // Note: This callback is fired at each app startup and whenever a new token is generated.
+            }
+        }
 
-@available(iOS 10, *)
-extension AppDelegate : UNUserNotificationCenterDelegate {
-    
-    // Receive displayed notifications for iOS 10 devices.
-    func userNotificationCenter(_ center: UNUserNotificationCenter,
-                                willPresent notification: UNNotification,
-                                withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-        let userInfo = notification.request.content.userInfo
-        
-        // With swizzling disabled you must let Messaging know about the message, for Analytics
-        // Messaging.messaging().appDidReceiveMessage(userInfo)
-        
-        // ...
-        
-        // Print full message.
-        print(userInfo)
-        
-        // Change this to your preferred presentation option
-        completionHandler([[.alert, .sound]])
-    }
-    
-    func userNotificationCenter(_ center: UNUserNotificationCenter,
-                                didReceive response: UNNotificationResponse,
-                                withCompletionHandler completionHandler: @escaping () -> Void) {
-        let userInfo = response.notification.request.content.userInfo
-        
-        // ...
-        
-        // With swizzling disabled you must let Messaging know about the message, for Analytics
-        // Messaging.messaging().appDidReceiveMessage(userInfo)
-        
-        // Print full message.
-        print(userInfo)
-        
-        completionHandler()
-    }
-}
-=======
->>>>>>> 2d5d69981c3cf2ac1ee7708806fd39953960a24c:SmileOnlineProject/files/AppDelegate.swift
+        @available(iOS 10, *)
+        extension AppDelegate : UNUserNotificationCenterDelegate {
+
+          // Receive displayed notifications for iOS 10 devices.
+          func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                      willPresent notification: UNNotification,
+            withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+            let userInfo = notification.request.content.userInfo
+
+            // With swizzling disabled you must let Messaging know about the message, for Analytics
+            // Messaging.messaging().appDidReceiveMessage(userInfo)
+
+            // ...
+
+            // Print full message.
+            print(userInfo)
+
+            // Change this to your preferred presentation option
+            completionHandler([[.alert, .sound]])
+          }
+
+          func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                      didReceive response: UNNotificationResponse,
+                                      withCompletionHandler completionHandler: @escaping () -> Void) {
+            let userInfo = response.notification.request.content.userInfo
+
+            // ...
+
+            // With swizzling disabled you must let Messaging know about the message, for Analytics
+            // Messaging.messaging().appDidReceiveMessage(userInfo)
+
+            // Print full message.
+            print(userInfo)
+
+            completionHandler()
+          }
+        }
